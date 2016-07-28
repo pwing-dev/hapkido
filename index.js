d792175b6be62131a850879216bca583ee3e5b3f
Lukas Böhm <lukas.boehm@gmail.com>
2016-07-28 20:47:27 +0200

    Add handlebars compilation

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const rollup  = require('express-middleware-rollup');
 const sass    = require('node-sass-middleware');
+const exphbs  = require('express-handlebars');
 const path    = require('path');
 const buble   = require('rollup-plugin-buble');
 
 const app = express();
+
+// Javascript compilation
 app.use(rollup({
   src: 'frontend/js',
   dest: 'static',
@@ -14,6 +17,8 @@ app.use(rollup({
     plugins: [ buble() ]
   }
 }));
+
+// Css compilation
 app.use(sass({
   root: __dirname,
   src:  'frontend/css',
@@ -21,5 +26,22 @@ app.use(sass({
   outputStyle: 'extended',
   prefix:  '/css'
 }));
+
+// Static file server
 app.use(express.static(path.join(__dirname, 'static')));
+
+// Templating
+app.set('views', 'frontend/html/');
+app.engine('.hbs', exphbs({
+  defaultLayout: 'default',
+  extname: '.hbs',
+  layoutsDir: 'frontend/html/layouts/',
+  partialsDir: 'frontend/html/partials/'
+}));
+app.set('view engine', '.hbs');
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+
 app.listen(3000);
