30a71b1977822993821427b509edb7fab1a362c0
Lukas Böhm <lukas.boehm@gmail.com>
2016-07-28 20:25:50 +0200

    Add sass middleware

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const rollup  = require('express-middleware-rollup');
+const sass    = require('node-sass-middleware');
 const path    = require('path');
 const buble   = require('rollup-plugin-buble');
 
@@ -12,6 +13,13 @@ app.use(rollup({
   rollupOpts: {
     plugins: [ buble() ]
   }
+}));
+app.use(sass({
+  root: __dirname,
+  src:  'frontend/sass',
+  dest: 'static',
+  outputStyle: 'extended',
+  prefix:  '/css'
 }));
 app.use(express.static(path.join(__dirname, 'static')));
 app.listen(3000);
