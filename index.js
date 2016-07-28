192039ca5a5e598851137e5572581ddf18fde0b9
Lukas Böhm <lukas.boehm@gmail.com>
2016-07-28 20:15:21 +0200

    Add buble to rollup build

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const rollup  = require('express-middleware-rollup');
 const path    = require('path');
- 
+const buble   = require('rollup-plugin-buble');
+
 const app = express();
 app.use(rollup({
   src: 'frontend/js',
   dest: 'static',
   root: __dirname,
-  prefix: '/js'
+  prefix: '/js',
+  rollupOpts: {
+    plugins: [ buble() ]
+  }
 }));
 app.use(express.static(path.join(__dirname, 'static')));
 app.listen(3000);
