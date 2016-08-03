35e08a86367088c66d3779aec1294d671b064dad
Moritz Hoffmann <moritz1@gmx.com>
2016-08-03 23:51:48 +0200

    Fix recaptcha in testcase

--- test/local-login.js
+++ test/local-login.js
@@ -7,6 +7,7 @@ const config       = require('config');
 
 const expect       = chai.expect;
 
+// fixed recaptcha response for testing
 const captchaResponse = '03AHJ_VuvX91Xbl1YlYf093zCTiyYCzBxbzfUxcbFm3aNr3tDjYLWGrTxgJKm62gThGu74QSs-JEzq4XTTpBVdjwEhaIMJUsbl2Utes8Df6DbAhJGQE_QkpNR3yTv10FTndzssZkqQAEKf7Q_Jwu3AGDNPekEV7ljU75HGTsHoXcncBlGUHwydn9wR3TmN8bOteYQLdgRCs8UKpOy4__IBcDK3zv651d35Ybf1aoudXbnGRI7eaucGnjVp7ZvEDDgDlHnrj4KLTm6tnU14cLPDDqDZBIADvhcDi6y6b7xxik5PSN-k6POg3EJd3la5mQh-Z2LWWCjVm8naulG0e0U6qt18JPgcodTquy6AVqgLcpkc33U2sIQ5ssiqkKQjmbXwMUnICtxanBUOZfs_N-cL86IfS97MfPLJule2z5ZikDu3uOavBwFGQ4O8TXI2_amlDhyqE0sxtZ5xnn3YxfAe3Ps6PlIU-r93-7RFp8227mXEo8ElG3lWp_8K1_Ak8nejS8ex0YZEGukZ2183PWL4ddGOnd6dzuotxZTO6SnRfF46Ae9RuBS5NCTPkOrbqr9QdgYQUv04OW-f3mO0nCl7dHdniu1Wbd4kTno0c7PL4EVm8rEhlUni8jT07y2TN1C8sV0cdtrRoQamlU5qnH6ED_3S5lBmJBky9QlV5x48PFs_Ez1f6hQit4YmM55JlXcyqqv8SuTW_0ObQ7N5oFH8NmvCUnev6StY5aiIT_W7AI_j7VDVCKdpIwIcWYIPpEYykYgDNAIkWo-7RuWBP_MAnas2BW0IXk-rHXVZizm8yzAk3PaydMJe-bCxT1EW7kcO65wHkP5tuh6YDPTbqD2RMVSk125yAsXSCwRIIHEngw4_UvMJVSNVtPAkCSsYh7euJUvY0NqEUDPVUR1dc8izpaP__TLtb_xQFRFXzMq_uT6pWrerUJOrcecHFjVaz-OEKm038XstG25gVwSmUfDaUJ7bIoCPHpBEgKlp0hnyR4-ujT5DMp_hE5g';
 
 // ATTENTION mocha binds `this` to some test case object, but this doesn't
@@ -14,7 +15,8 @@ const captchaResponse = '03AHJ_VuvX91Xbl1YlYf093zCTiyYCzBxbzfUxcbFm3aNr3tDjYLWGr
 // everywhere. Thank you
 
 describe('Local login', function() {
-  config.recaptcha = {
+  // set recaptcha keys to test-keys
+  config.server.recaptcha = {
     site_key: '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI',
     secret_key: '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe',
   };
@@ -26,10 +28,7 @@ describe('Local login', function() {
     mockgoose(mongoose)
       .then(
         () => {
-          const mockgooserequirefrom = proxyquire('requirefrom', {
-            'config': config,
-            'mongoose': mongoose
-          });
+          const mockgooserequirefrom = proxyquire('requirefrom', {'mongoose': mongoose});
           Account = mockgooserequirefrom('server/models')('account');
           // create an app instance
           return mockgooserequirefrom('server')('server')();
