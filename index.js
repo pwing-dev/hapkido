ed5d160988bb95a0897aa443fa8b86af5bafaf08
Lukas Böhm <suluke93@gmail.com>
2016-08-06 21:40:55 +0200

    Fix wrong config path for server path in index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const config       = require('config');
-const createServer = require('./server/server');
+const createServer = require('requirefrom')('server')('server');
 
 if (require.main === module) {
   // called directly from cli
   createServer().then(
-    app => app.listen(config.get('server.port')),
+    app => app.listen(config.get('port')),
     error => { console.error(error); process.exit(1); }
   );
 } else {
