fed90110b9bba5bb6c95c8f39b067657edc7f78f
Lukas Böhm <suluke93@gmail.com>
2016-08-10 00:21:53 +0200

    Wrap models/application in a self-executing function
    I did this as a preparation for dropping the mongoose singleton.
    But we are still uncertain if we really want to do this.
    Therefore I just leave it as is and if we decide we do it, it will be very easy with this code

--- server/models/application.js
+++ server/models/application.js
@@ -1,7 +1,7 @@
-const mongoose     = require('mongoose');
 const findOrCreate = require('mongoose-findorcreate');
 const randomstring = require('randomstring');
-const Schema       = mongoose.Schema;
+const mongoose     = require('mongoose');
+const Schema       = require('mongoose').Schema;
 
 const Application = new Schema({
   setupComplete: { type: Boolean, default: false },
@@ -9,58 +9,61 @@ const Application = new Schema({
 });
 Application.plugin(findOrCreate);
 
-const State = mongoose.model('Application', Application);
-const injectState = (() => {
-  // cache an already resolved state
-  let state = new Promise((resolve, reject) => {
-    State.findOrCreate({}, (err, doc) => {
-      if (err) {
-        reject(err);
-        return;
-      }
-      state = doc;
-      resolve(doc);
+module.exports = (() => {
+  const State = mongoose.model('Application', Application);
+  const injectState = (() => {
+    // cache an already resolved state
+    let state = new Promise((resolve, reject) => {
+      State.findOrCreate({}, (err, doc) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        state = doc;
+        resolve(doc);
+      });
     });
+    return callback => {
+      Promise.resolve(state).then(val => callback(null, val), err => callback(err));
+    };
+  })();
+
+  const makeSyncApi = state => ({
+    isSetupComplete: () => state.setupComplete,
+    getSessionSecret: () => state.sessionSecret
   });
-  return callback => {
-    Promise.resolve(state).then(val => callback(null, val), err => callback(err));
-  };
-})();
 
-const makeSyncApi = state => ({
-  isSetupComplete: () => state.setupComplete,
-  getSessionSecret: () => state.sessionSecret
-});
-
-module.exports = {
-  assertInitialized: callback => injectState((err, state) => {
-    if (err) {
-      callback(err);
-    } else {
-      callback(null, makeSyncApi(state));
-    }
-  }),
-  promiseInitialized: () => new Promise((resolve, reject) => {
-    module.exports.assertInitialized((err, api) => {
+  const API = {
+    assertInitialized: callback => injectState((err, state) => {
       if (err) {
-        reject(err);
+        callback(err);
       } else {
-        resolve(api);
+        callback(null, makeSyncApi(state));
       }
-    });
-  })
-};
-
-// generate asynchronous api
-const dummyApi = makeSyncApi();
-Object.keys(dummyApi).forEach(fun => {
-  module.exports[fun] = (...args) => {
-    const callback = args.pop();
-    module.exports.promiseInitialized().then(api => callback(null, api[fun](...args)), callback);
+    }),
+    promiseInitialized: () => new Promise((resolve, reject) => {
+      module.exports.assertInitialized((err, api) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(api);
+        }
+      });
+    })
   };
-});
 
-module.exports.setSetupComplete = done => injectState((err, state) => {
-  state.setupComplete = true;
-  state.save(done);
-});
+  // generate asynchronous api
+  const dummyApi = makeSyncApi();
+  Object.keys(dummyApi).forEach(fun => {
+    API[fun] = (...args) => {
+      const callback = args.pop();
+      API.promiseInitialized().then(api => callback(null, api[fun](...args)), callback);
+    };
+  });
+
+  API.setSetupComplete = done => injectState((err, state) => {
+    state.setupComplete = true;
+    state.save(done);
+  });
+  return API;
+})();
